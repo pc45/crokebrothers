d8aa2dc395c6d1e020b9487da24679ca399e0f88
pc45 <pete@petecroke.com>
2022-11-02 17:18:48 -0500

    copy update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ export function About() {
             About
           </p>
           <p className="mt-4 text-lg tracking-tight text-slate-700">
-            CROKE & COMPANY exists to support public interest causes and progressive campaigns. We harness decades of strategic public communications, video production and web development services to help our clients thrive.
+            Croke & Co. exists to support public interest causes and progressive campaigns. We harness decades of strategic public communications, video production and web development experience to help our clients thrive.
           </p>
         </div>
       </Container>
