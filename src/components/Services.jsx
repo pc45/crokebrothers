1cf5d34a762d3a3505026987fda8a55badbfb070
pc45 <pete@petecroke.com>
2022-11-25 22:44:20 -0600

    service update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,9 +7,8 @@ import {
 
 const features = [
   {
-    name: 'Strategic & Crisis Communication',
-    description: 'Coaching, Strategic & Crisis Communication\n' +
-        'We offer advice and expertise to inform action and decisionmaking, and can lead workshops and training for executives and boards to help navigate your priorities. Want to share good news, impact public understanding of an issue, service, or product, or get ahead of a looming concern? Whether you need to build a coalition, change, a reputation, or move an audience, we will customize a strategic plan and simplify its execution - so you can stay focused on what matters most.',
+    name: 'Coaching, Strategic & Crisis Communication',
+    description: 'We offer advice and expertise to inform action and decisionmaking, and can lead workshops and training for executives and boards to help navigate your priorities. Want to share good news, impact public understanding of an issue, service, or product, or get ahead of a looming concern? Whether you need to build a coalition, change, a reputation, or move an audience, we will customize a strategic plan and simplify its execution - so you can stay focused on what matters most.',
     icon: ChatBubbleBottomCenterIcon,
   },
   {
