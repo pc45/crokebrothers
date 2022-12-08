c95d5ebd05e1d9066f17daefa3ab56dee068385a
pc45 <pete@petecroke.com>
2022-12-08 10:03:34 -0600

    fix wrong environment var reference

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -6,8 +6,8 @@ let lodash = require('lodash')
 let aws = require('@aws-sdk/client-ses')
 
 export default function (req, res) {
-  const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY
-  const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY
+  const AWS_ACCESS_KEY_ID = process.env.CB_AWS_ACCESS_KEY
+  const AWS_SECRET_ACCESS_KEY = process.env.CB_AWS_SECRET_ACCESS_KEY
   const FROM_ADDRESS = 'no-reply@crokeandco.com'
   const TO_ADDRESS = process.env.MAIL_RECIPIENTS
   const services = lodash.compact(req.body.serviceInfo)
@@ -16,8 +16,8 @@ export default function (req, res) {
     apiVersion: '2010-12-01',
     region: 'us-east-1',
     credentials: {
-      secretAccessKey: CB_AWS_SECRET_ACCESS_KEY,
-      accessKeyId: CB_AWS_ACCESS_KEY_ID,
+      secretAccessKey: AWS_SECRET_ACCESS_KEY,
+      accessKeyId: AWS_ACCESS_KEY_ID,
     },
   })
 
