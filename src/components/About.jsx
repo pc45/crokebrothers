1b98b0de89cbad6d1b3e28d36fab0830a21be26a
pc45 <pete@petecroke.com>
2022-12-09 17:45:31 -0600

    update copy

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,14 +10,14 @@ export function About() {
             About Us
           </h2>
           <p className=" mt-4 text-lg tracking-tight text-slate-700">
-            Croke & Company is a strategic communications firm that offers video
-            production, web design, polling, and research to organizations and
+            Croke & Company offers video production, web design, polling,
+            research, and related strategic guidance to organizations and
             individuals who are dedicated to the public interest and the common
             good. We are specially equipped to help progressive causes,
             companies, and campaigns thrive. We cut through nonsense and noise -
             to widen your reach, amplify your success, and prepare you to
             navigate any public challenge. We believe in using plain language,
-            elegant design, and humor to support our partners. Our goal is to
+            clean design, and good humor to support our partners. Our goal is to
             harness shared expertise to help build a better, safer, more
             inclusive world.
           </p>
